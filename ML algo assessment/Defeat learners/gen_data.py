cbe90547b21dd06d75ea2b8e509aee7e33270d70
Kwabena16108 <67174225+Kwabena16108@users.noreply.github.com>
2021-11-04 11:51:36 -0400

    Update gen_data.py

--- ML algo assessment/Defeat learners/gen_data.py
+++ ML algo assessment/Defeat learners/gen_data.py
@@ -1,30 +1,3 @@
-""""""
-"""  		  	   		   	 		  		  		    	 		 		   		 		  
-template for generating data to fool learners (c) 2016 Tucker Balch  		  	   		   	 		  		  		    	 		 		   		 		  
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		   	 		  		  		    	 		 		   		 		  
-Atlanta, Georgia 30332  		  	   		   	 		  		  		    	 		 		   		 		  
-All Rights Reserved  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-Template code for CS 4646/7646  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		   	 		  		  		    	 		 		   		 		  
-works, including solutions to the projects assigned in this course. Students  		  	   		   	 		  		  		    	 		 		   		 		  
-and other users of this template code are advised not to share it with others  		  	   		   	 		  		  		    	 		 		   		 		  
-or to make it available on publicly viewable websites including repositories  		  	   		   	 		  		  		    	 		 		   		 		  
-such as github and gitlab.  This copyright statement should not be removed  		  	   		   	 		  		  		    	 		 		   		 		  
-or edited.  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-We do grant permission to share solutions privately with non-students such  		  	   		   	 		  		  		    	 		 		   		 		  
-as potential employers. However, sharing with other current or future  		  	   		   	 		  		  		    	 		 		   		 		  
-students of CS 7646 is prohibited and subject to being investigated as a  		  	   		   	 		  		  		    	 		 		   		 		  
-GT honor code violation.  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
------do not edit anything above this line---  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-Student Name: Dickson Nkwantabisa 		  	   		   	 		  		  		    	 		 		   		 		  
-GT User ID: dnkwantabisa3   	   		   	 		  		  		    	 		 		   		 		  
-GT ID: 903658462 	  	   		   	 		  		  		    	 		 		   		 		  
-"""
 
 import math
 
@@ -35,11 +8,7 @@ import numpy as np
 # better for linear regression than decision trees
 
 def best_4_lin_reg(seed=1489683273):
-    """  		  	   		   	 		  		  		    	 		 		   		 		  
-    Returns data that performs significantly better with LinRegLearner than DTLearner.  		  	   		   	 		  		  		    	 		 		   		 		  
-    The data set should include from 2 to 10 columns in X, and one column in Y.  		  	   		   	 		  		  		    	 		 		   		 		  
-    The data should contain from 10 (minimum) to 1000 (maximum) rows.  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
+    """  		  	   		   	 		  		  		    	 		 		   		 		  		  	   		   	 		  		  		    	 		 		   		 		  
     :param seed: The random seed for your data generation.  		  	   		   	 		  		  		    	 		 		   		 		  
     :type seed: int  		  	   		   	 		  		  		    	 		 		   		 		  
     :return: Returns data that performs significantly better with LinRegLearner than DTLearner.  		  	   		   	 		  		  		    	 		 		   		 		  
@@ -67,11 +36,7 @@ def best_4_lin_reg(seed=1489683273):
 
 
 def best_4_dt(seed=1489683273):
-    """  		  	   		   	 		  		  		    	 		 		   		 		  
-    Returns data that performs significantly better with DTLearner than LinRegLearner.  		  	   		   	 		  		  		    	 		 		   		 		  
-    The data set should include from 2 to 10 columns in X, and one column in Y.  		  	   		   	 		  		  		    	 		 		   		 		  
-    The data should contain from 10 (minimum) to 1000 (maximum) rows.  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
+    """  		  	   		   	 		  		  		    	 		 		   		 		   		  	   		   	 		  		  		    	 		 		   		 		  
     :param seed: The random seed for your data generation.  		  	   		   	 		  		  		    	 		 		   		 		  
     :type seed: int  		  	   		   	 		  		  		    	 		 		   		 		  
     :return: Returns data that performs significantly better with DTLearner than LinRegLearner.  		  	   		   	 		  		  		    	 		 		   		 		  
@@ -85,15 +50,3 @@ def best_4_dt(seed=1489683273):
     y = x[:, 0]**3 + x[:, 1]*x[:, -1] + x[:, 4]
 
     return x, y
-
-
-def author():
-    """  		  	   		   	 		  		  		    	 		 		   		 		  
-    :return: The GT username of the student  		  	   		   	 		  		  		    	 		 		   		 		  
-    :rtype: str  		  	   		   	 		  		  		    	 		 		   		 		  
-    """
-    return "dnkwantabisa3"  		  	   		   	 		  		  		    	 		 		   		 		  
-
-
-# if __name__ == "__main__":
-#     print("they call me Tim.")
