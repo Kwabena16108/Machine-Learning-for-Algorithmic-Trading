10c7c821db8d323e58493cffc2ef35b0548d2afa
Kwabena16108 <67174225+Kwabena16108@users.noreply.github.com>
2021-11-10 13:55:57 -0500

    Update QLearner.py

--- ML algo assessment/Learners/QLearner.py
+++ ML algo assessment/Learners/QLearner.py
@@ -1,33 +1,3 @@
-""""""  		  	   		   	 		  		  		    	 		 		   		 		  
-"""  		  	   		   	 		  		  		    	 		 		   		 		  
-Template for implementing QLearner  (c) 2015 Tucker Balch  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		   	 		  		  		    	 		 		   		 		  
-Atlanta, Georgia 30332  		  	   		   	 		  		  		    	 		 		   		 		  
-All Rights Reserved  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-Template code for CS 4646/7646  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		   	 		  		  		    	 		 		   		 		  
-works, including solutions to the projects assigned in this course. Students  		  	   		   	 		  		  		    	 		 		   		 		  
-and other users of this template code are advised not to share it with others  		  	   		   	 		  		  		    	 		 		   		 		  
-or to make it available on publicly viewable websites including repositories  		  	   		   	 		  		  		    	 		 		   		 		  
-such as github and gitlab.  This copyright statement should not be removed  		  	   		   	 		  		  		    	 		 		   		 		  
-or edited.  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-We do grant permission to share solutions privately with non-students such  		  	   		   	 		  		  		    	 		 		   		 		  
-as potential employers. However, sharing with other current or future  		  	   		   	 		  		  		    	 		 		   		 		  
-students of CS 7646 is prohibited and subject to being investigated as a  		  	   		   	 		  		  		    	 		 		   		 		  
-GT honor code violation.  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
------do not edit anything above this line---  		  	   		   	 		  		  		    	 		 		   		 		  
-  		  	   		   	 		  		  		    	 		 		   		 		  
-Student Name: Dickson Nkwantabisa
-GT User ID: dnkwantabisa3
-GT ID: 903658462			  	   		   	 		  		  		    	 		 		   		 		  
-"""  		  	   		   	 		  		  		    	 		 		   		 		  
-
-
 
 import random as rand
 import numpy as np
@@ -154,10 +124,6 @@ class QLearner(object):
         if self.verbose:
             print(f"s = {s_prime}, a = {action}, r={r}")
         return action
-
-    def author(self):
-        return "dnkwantabisa3"
-
 
 if __name__ == "__main__":
     print("Remember Q from Star Trek? Well, this isn't him")
