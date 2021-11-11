ee9734f94454118c8422a9909f4032df2a92f15b
Kwabena16108 <67174225+Kwabena16108@users.noreply.github.com>
2021-11-10 20:03:16 -0500

    Update testproject.py

--- trading strategies/theoretically optimal strategy/testproject.py
+++ trading strategies/theoretically optimal strategy/testproject.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-		  		  		    	 		 		   		 		  
-"""
-Student Name: Dickson Nkwantabisa
-GT User ID: dnkwantabisa3
-GT ID: 903658462		  	   		   	 		  		  		    	 		 		   		 		   
-""" 	
-  
+ 
 import datetime as dt
 from util import get_data
 from marketsimcode import compute_portvals
@@ -15,12 +9,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import indicators as ind
-
-#%%
-def author():
-    return 'dnkwantabisa3'
-
-#%%
 
 
 def testproject():
